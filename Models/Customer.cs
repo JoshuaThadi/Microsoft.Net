7591f205a660ca0e2723b251689df4e67784cd7a
MASTERJUDAH <141491462+JoshuaThadi@users.noreply.github.com>
2025-05-08 11:35:06 +0530

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerManagement.Models
+{
+    public class Customer : UserActivity
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
